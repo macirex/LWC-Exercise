4f5220a93b32f1ead23c086da089a15ed746b60d
miguel <mialchrex@gmail.com>
2021-03-08 23:35:18 -0300

    User-case-2: moved folders to the root directory

---
+++ force-app/main/default/lwc/availableProducts/availableProducts.js
@@ -0,0 +1,99 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import Pricebook2Id from '@salesforce/schema/order.Pricebook2Id';
+import { getRecord } from 'lightning/uiRecordApi';
+import { refreshApex } from '@salesforce/apex';
+import getAvailableProducts from '@salesforce/apex/AvailableProductsController.getAvailableProducts';
+
+const FIELDS = [
+    Pricebook2Id,
+];
+
+
+const columns = [
+    {
+        label: 'Name',
+        fieldName: 'Name',
+        type: 'text',
+        sortable: true
+    },
+    {
+        label: 'List Price',
+        fieldName: 'UnitPrice',
+        sortable: true,
+        type: 'currency',
+        typeAttributes: { currencyCode: 'EUR', step: '0.001'}
+    },{
+        label: 'Add Product',
+        type: 'button-icon',
+        initialWidth: 75,
+        typeAttributes: {
+            iconName: 'utility:cart',
+            title: 'Add Product',
+            variant: 'border-filled',
+            alternativeText: 'Add Product'
+        }
+    }
+    ];
+
+
+export default class AvailableProducts extends LightningElement {
+    @api recordId;
+    
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
+    order;
+    @track value;
+    @track error;
+    @api sortedDirection = 'asc';
+    @api sortedBy = 'Name';
+    @api searchKey = '';
+   
+    @track data = []; 
+    @track columns; 
+
+    @wire(getAvailableProducts, {   pricebook2Id: '$order.data.fields.Pricebook2Id.value' 
+        ,searchKey: '$searchKey', sortBy: '$sortedBy', sortDirection: '$sortedDirection'})
+    wiredAvailableProducts({ error, data }) {
+        if (data) {
+            debugger;
+            let jsonData = JSON.parse(data);
+            this.data = jsonData;
+            this.columns = columns;
+            this.error = undefined;
+        } else if (error) {
+            debugger;
+
+            this.error = error;
+            this.data = undefined;
+        }
+    }
+
+    sortColumns( event ) {
+        this.sortedBy = event.detail.fieldName;
+        this.sortedDirection = event.detail.sortDirection;
+        return refreshApex(this.result);
+        
+    }
+  
+    handleKeyChange( event ) {
+        if(event.target.value.length >= 3){
+            this.searchKey = event.target.value;
+            return refreshApex(this.result);
+        }
+    }
+    isLoading() {
+        return !this.wiredProperty.data && !this.wiredProperty.error;
+    }
+
+    callRowAction( event ) {  
+        debugger;
+        const recId =  event.detail.row.Id;
+        const title =  event.detail.action.title;  
+
+        if ( recId && title && title === 'Add Product' ) {  
+            debugger;
+        }          
+      
+    }  
+     
+    
+}
